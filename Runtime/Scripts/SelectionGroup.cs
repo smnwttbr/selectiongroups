ab414f580c9ef5f35df6888f9425160e1a38831a
Simon Wittber <simonwittber@unity3d.com>
2024-12-09 12:33:54 +0800

    Update Runtime/Scripts/SelectionGroup.cs
    Co-authored-by: chrstphrsxtn <74899224+chrstphrsxtn@users.noreply.github.com>

--- Runtime/Scripts/SelectionGroup.cs
+++ Runtime/Scripts/SelectionGroup.cs
@@ -14,7 +14,7 @@ namespace Unity.SelectionGroups
     /// A component to group a number of GameObjects under a common name.
     /// </summary>
     /// <remarks>
-    /// The SelectionGroup component manages a list of GameObjects that are part of the group. It is created and managed using the Selection Group window.
+    /// The SelectionGroup component manages a list of GameObjects that are part of the group. It's created and managed using the **Selection Groups** window.
     /// The members of a SelectionGroup are created manually in the editor by adding GameObjects to the group, or automatically by using a GoQL query string. If a GoQL query string is used, the group will automatically include GameObjects from the hierarchy that match the query. The <see cref="IsAutoFilled"/> method can be used to determine if the group is automatically filled.
     ///
     /// If you want to iterate over the GameObjects in the group, you can use the <see cref="Members"/> property. You can also access members by index
