7ae505b947063dcab06b4eb5f1212963612b709f
Simon Wittber <simonwittber@unity3d.com>
2024-12-10 11:18:34 +0800

    doc: Updated SelectionGroup class documentation (#313)
    * doc update
    
    * Update Runtime/Scripts/SelectionGroup.cs
    
    Co-authored-by: chrstphrsxtn <74899224+chrstphrsxtn@users.noreply.github.com>
    
    * Update Runtime/Scripts/SelectionGroup.cs
    
    Co-authored-by: chrstphrsxtn <74899224+chrstphrsxtn@users.noreply.github.com>
    
    * Update Runtime/Scripts/SelectionGroup.cs
    
    Co-authored-by: chrstphrsxtn <74899224+chrstphrsxtn@users.noreply.github.com>
    
    * Update Runtime/Scripts/SelectionGroup.cs
    
    Co-authored-by: chrstphrsxtn <74899224+chrstphrsxtn@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: chrstphrsxtn <74899224+chrstphrsxtn@users.noreply.github.com>

--- Runtime/Scripts/SelectionGroup.cs
+++ Runtime/Scripts/SelectionGroup.cs
@@ -13,6 +13,21 @@ namespace Unity.SelectionGroups
     /// <summary>
     /// A component to group a number of GameObjects under a common name.
     /// </summary>
+    /// <remarks>
+    /// The SelectionGroup component manages a list of GameObjects that are part of the group. It's created and managed using the **Selection Groups** window.
+    /// The members of a SelectionGroup are created manually in the Editor by adding GameObjects to the group, or automatically by using a GoQL query string. If a GoQL query string is used, the group automatically includes GameObjects from the hierarchy that match the query. The <see cref="IsAutoFilled"/> method can be used to determine if the group is automatically filled.
+    ///
+    /// You can use the <see cref="Members"/> property to iterate over the GameObjects in the group.
+    /// using array syntax. For example, <c>group[0]</c> returns the first GameObject in the group. The <see cref="Count"/> property returns the number of GameObjects in the group.
+    /// </remarks>
+    /// <example>
+    ///     This is how you can access the members of a SelectionGroup:
+    ///     <code lang="cs"><![CDATA[
+    ///         foreach(var member in selectionGroup.Members) {
+    ///             // Do something with member
+    ///         }
+    ///     ]]></code>
+    /// </example>
     [ExecuteAlways]
     [AddComponentMenu("")]
     public class SelectionGroup : MonoBehaviour, IList<GameObject>, ISerializationCallbackReceiver    
